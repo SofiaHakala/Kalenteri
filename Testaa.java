2d71102b53bcb35a84c8b5806b11ddb7b4406c4d
camilla hänninen <36483624+catahan@users.noreply.github.com>
2018-02-15 19:04:10 +0200

    Update Testaa.java

--- Testaa.java
+++ Testaa.java
@@ -6,16 +6,17 @@ public class Testaa {
   
   /**
    * kesken olevaa:
-   * -kun tapahtumia etsit‰‰n, ohjelman ei pit‰isi k‰ytt‰‰ "tapahtumat" listaa (ei s‰ily ohjelman sulkeuduttua), vaan etsi‰ "tapahtumat"-kansiosta tiedostoja. 
-   * ohjelma voisi tallentaa tiedoston ekalle riville p‰iv‰m‰‰r‰n oikeassa muodossa. ohjelma siis lukisi kaikkien kansiosta lˆytyvien tiedostojen ekan rivin
-   * ja vertailisi sit‰ etsitt‰v‰‰n p‰iv‰m‰‰r‰‰n.
-   * -muisiinpanoja ei olla toteutettu ollenkaan. eli ne tulisi tallentaa myˆs tiedostoon (jonka nimi on siis tapahtuman nimi)
-   * -olisi ihan kiva ominaisuus voida muokata tapahtuman nime‰ (tiedoston nime‰) ja muistiinpanoja (tiedoston sis‰ltˆ‰)
-   * -jotenkin toimintojen valitsemista voisi tehd‰ jouhevammaksi. esim. se, ett‰ enteri‰ on pakko aina painaa voitaisiin ehk‰ vain poistaa? vai olisiko ehk‰ ihan hyv‰ ominaisuus
-   * -p‰iv‰ysten muodot. eli nyt p‰iv‰ykset ovat muodossa mm/dd/yy, mit‰ ei suomessa paljoa k‰ytet‰
-   * -virhedenk‰sittely‰ voitaisiin myˆs v‰h‰n hienos‰‰t‰‰
-   * -poista tapahtumia-ominaisuus ei n‰yt‰ toimivan, en ole paljoa ehtinyt sit‰ muuttaa vanhasta ohjelmasta mist‰ tuon otin. pit‰isi siis voida poistaa tapahtumia p‰iv‰m‰‰r‰n syˆtt‰m‰ll‰
-   * -jotain muutakin muutettavaa on kai, lis‰ill‰‰n t‰nne sitten
+   * -kun tapahtumia etsit√§√§n, ohjelman ei pit√§isi k√§ytt√§√§ "tapahtumat" listaa (ei s√§ily ohjelman sulkeuduttua), vaan etsi√§ "tapahtumat"-kansiosta tiedostoja. 
+   * ohjelma voisi tallentaa tiedoston ekalle riville p√§iv√§m√§√§r√§n oikeassa muodossa. ohjelma siis lukisi kaikkien kansiosta l√∂ytyvien tiedostojen ekan rivin
+   * ja vertailisi sit√§ etsitt√§v√§√§n p√§iv√§m√§√§r√§√§n.
+   * -muisiinpanoja ei olla toteutettu ollenkaan. eli ne tulisi tallentaa my√∂s tiedostoon (jonka nimi on siis tapahtuman nimi)
+   * -olisi ihan kiva ominaisuus voida muokata tapahtuman nime√§ (tiedoston nime√§) ja muistiinpanoja (tiedoston sis√§lt√∂√§)
+   * -jotenkin toimintojen valitsemista voisi tehd√§ jouhevammaksi. esim. se, ett√§ enteri√§ on pakko aina painaa voitaisiin ehk√§ vain poistaa? vai olisiko ehk√§ ihan hyv√§ ominaisuus
+   * -p√§iv√§ysten muodot. eli nyt p√§iv√§ykset ovat muodossa mm/dd/yy, mit√§ ei suomessa paljoa k√§ytet√§
+   * -virhedenk√§sittely√§ voitaisiin my√∂s v√§h√§n hienos√§√§t√§√§
+   * -joissain luokissa taisi olla turhia import-lauseita jotka voisi poistaa
+   * -poista tapahtumia-ominaisuus ei n√§yt√§ toimivan, en ole paljoa ehtinyt sit√§ muuttaa vanhasta ohjelmasta mist√§ tuon otin. pit√§isi siis voida poistaa tapahtumia p√§iv√§m√§√§r√§n sy√∂tt√§m√§ll√§
+   * -jotain muutakin muutettavaa on kai, lis√§ill√§√§n t√§nne sitten
    * */
 
     public static void main(String[] args) throws IOException{
@@ -28,7 +29,7 @@ public class Testaa {
             kalenteri.tulostaAlkuvalikko(); 
             System.out.println();
             System.out.println("Valitse toiminnallisuus:");
-            System.out.println("1 - Lis‰‰ tapahtuma");
+            System.out.println("1 - Lis√§√§ tapahtuma");
             System.out.println("2 - Etsi tapahtumia");
             System.out.println("3 - Poista tapahtumia");
             System.out.println("4 - Poistu");
@@ -46,10 +47,10 @@ public class Testaa {
                     kalenteri.poistaTapahtumia();
                     break;
                 case "4":
-                    System.out.println("Paina n‰pp‰int‰ poistuaksesi sovelluksesta");
+                    System.out.println("Paina n√§pp√§int√§ poistuaksesi sovelluksesta");
                     break;
                 default:
-                    System.out.println("Tapahtui virhe, valitse jokin annetuista vaihtoehdoista syˆtt‰m‰lle toiminnolle kuuluva numero");
+                    System.out.println("Tapahtui virhe, valitse jokin annetuista vaihtoehdoista sy√∂tt√§m√§lle toiminnolle kuuluva numero");
                     break;
             }
         }
