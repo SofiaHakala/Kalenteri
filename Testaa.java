c113b2b1a7aee85b9970f36afae6ddf0d633124b
camilla hänninen <36483624+catahan@users.noreply.github.com>
2018-02-15 19:57:16 +0200

    Update Testaa.java

--- Testaa.java
+++ Testaa.java
@@ -9,18 +9,29 @@ public class Testaa {
    * - Kun tapahtumia etsitään (etsiTapahtumia()), ohjelman ei pitäisi käyttää "tapahtumat" listaa (ei säily ohjelman sulkeuduttua), 
    * vaan etsiä "tapahtumat"-kansiosta tiedostoja. Ohjelma voisi vaikka tallentaa tiedoston ekalle riville päivämäärän oikeassa 
    * muodossa. Ohjelma siis lukisi kaikkien kansiosta löytyvien tiedostojen ekan rivn ja vertailisi sitä etsittävään päivämäärään.
+   *
    * - Muisiinpanoja ei olla toteutettu ollenkaan. Eli ne tulisi tallentaa myös tiedostoon (jonka nimi on siis tapahtuman nimi)
+   *
    * - Olisi ihan kiva ominaisuus voida muokata tapahtuman nimeä (tiedoston nimeä) ja muistiinpanoja (tiedoston sisältöä)
+   *
    * - Joidenkin toimintojen valitsemista voisi tehdä jouhevammaksi. esim. riippuvuus enter-näppäimeen.
+   *
    * - Päiväysten muodot. eli nyt päiväykset ovat muodossa mm/dd/yy, mitä ei suomessa paljoa käytetä.
+   *
    * - Virhedenkäsittelyä voitaisiin myös vähän hienosäätää
+   *
    * - Joissain luokissa taisi olla turhia import-lauseita jotka voisi poistaa (tai en ole oikeastaan tarkistanut)
+   *
    * - Poista tapahtumia-ominaisuus ei näytä toimivan, en ole paljoa ehtinyt sitä muuttaa vanhasta ohjelmasta mistä tuon otin. 
    * Pitäisi siis voida poistaa tapahtumia päivämäärän syöttämällä. Myös tapahtumasta tehty tiedosto pitäisi poistaa.
+   *
    * - Pari muuta ominaisuutta pitäisi saada toimimaan ilman riippuvuutta "tapahtumat"-listaan. Eli ne, joiden pitäisi pystyä hakemaan 
    * tietoa tapahtumista ohjelman sulkemisenkin jälkeen.
+   *
    * - "tapahtumat"-alikansion automaattinen luonti pitäisi tehdä, ja myös tarkistaa onko kansio jo olemassa yms.
+   *
    * - Dokumentoinnin muuttaminen selkeämmäksi ennen työn palautusta
+   *
    * - Jotain muutakin muutettavaa on kai, lisäillään tänne sitten
    * */
 
